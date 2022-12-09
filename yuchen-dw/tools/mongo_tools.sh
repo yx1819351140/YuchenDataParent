9ee3b9b72225b8ec86f49b29137796fadd8cdba6
xiaozhennan <xiao_zhennan@126.com>
2022-12-09 15:11:20 +0800

    [feat][yuchen-dw]:mongo_tools.sh导出支持自动推断导出文件类型

--- yuchen-dw/tools/mongo_tools.sh
+++ yuchen-dw/tools/mongo_tools.sh
@@ -15,6 +15,7 @@ source ${SCRIPT_HOME}/env/common_setting.sh
 
 
 run_flag=0
+#默认的导出mongo配置, 如果需要链接其它mongo, 需要在脚本中 export mongo_profile=xxxx.properties
 mprofile=$CONF_DIR/$PROJECT_ENV/mongo.properties
 if [ ! -n "$mongo_profile" ]; then
 	mporfile=$mongo_profile
@@ -26,24 +27,37 @@ mongo_password=`$TOOLS_DIR/load_config.sh $mprofile mongo.password`
 mongo_user=`$TOOLS_DIR/load_config.sh $mprofile mongo.user`
 mongo_auth_db=`$TOOLS_DIR/load_config.sh $mprofile mongo.authdb`
 
-export_cmd="mongoexport -u $mongo_user -h $mongo_server -p $mongo_password --authenticationDatabase $mongo_auth_db --type=json --jsonFormat=canonical --noHeaderLine "
+#定义默认导出的数据文件格式
+export_type=json
+#默认的导出命令
+export_cmd="mongoexport -u $mongo_user -h $mongo_server -p $mongo_password --authenticationDatabase $mongo_auth_db --jsonFormat=canonical --noHeaderLine "
 
-#执行sql
+#执行导出数据
 exec_export_data ()
 {
+#数据库
 database="${1}"
+#表
 collection="${2}"
+#导出文件
 export_file="${3}"
+#导出字段
 fields="${4}"
+#默认查询语句
 query="{}"
 if [ -n "${@:5}" ]; then
-	echo "queryyy""${@:5}"
+	echo "query:""${@:5}"
 	query="${@:5}"
 fi
+# shellcheck disable=SC2006
+# 根据导出文件后缀区分导出类型,支持json和csv
+export_type=`echo "${export_file}" | grep -q -E '\.json$' && echo "json" || echo "csv"`
+#创建查询语句临时文件
 queryFile=`mktemp -t mongo_query.json.XXXXXXXXXXXX`
+#查询语句写入到查询语句的临时文件
 echo $query > $queryFile
 echo "fields:  $fields"
-export_cmd=$export_cmd"-d $database -c $collection --queryFile $queryFile -o $export_file -f $fields"
+export_cmd=$export_cmd"--type=$export_type -d $database -c $collection --queryFile $queryFile -o $export_file -f $fields"
 echo $export_cmd
 #export_cmd=${export_cmd%--*}
 logger_info "开始进行mongo数据导出, 数据库: [$database], 数据表: [$collection], 查询条件: [$query]"
@@ -52,13 +66,11 @@ flag_num=$?
 rm $queryFile
 logger_info "导出mongo数据完成, 数据导出到: [$export_file]"
 return $flag_num
-	
-echo "exec export data"
 }
 
 exec_import_data ()
 {
-echo "exec import data"
+echo "Importing data to mongo is not currently supported!"
 }
 
 
