c8f9115c44dda9f4062fc50051a5085d8e07581c
qipeng Hu <qipenghu@126.com>
2023-01-09 15:43:05 +0800

    [chore][yuchen-monitor]:配置文件修改

--- yuchen-monitor/src/main/java/com/yuchen/data/monitor/tasks/SchedulerProd.java
+++ yuchen-monitor/src/main/java/com/yuchen/data/monitor/tasks/SchedulerProd.java
@@ -37,17 +37,17 @@ public class SchedulerProd {
 		TopicMonitor(kafkaConsumer,monitorTopicName,monitorGroupName,consumeThresholdTime,produceThresholdTime);
 	}
 
-//	@Scheduled(cron="${prod.kafka.globalEventNLPResult.cron}")
-//	public void globalEventNLPResultMonitor() throws IOException {
-//		org.apache.kafka.clients.consumer.KafkaConsumer kafkaConsumer = monitorConfig.getKafkaConsumer();
-//		val monitorObject = kafkaMonitor.getGlobalEventNLPResult();
-//		String monitorTopicName = monitorObject.getTopic();
-//		logger.info("监控主题为：{}", monitorTopicName);
-//		String monitorGroupName = monitorObject.getGroup();
-//		Integer consumeThresholdTime = monitorObject.getConsumeThresholdTime();
-//		Integer produceThresholdTime = monitorObject.getProduceThresholdTime();
-//		TopicMonitor(kafkaConsumer,monitorTopicName,monitorGroupName,consumeThresholdTime,produceThresholdTime);
-//	}
+	@Scheduled(cron="${prod.kafka.globalEventNLPResult.cron}")
+	public void globalEventNLPResultMonitor() throws IOException {
+		org.apache.kafka.clients.consumer.KafkaConsumer kafkaConsumer = monitorConfig.getKafkaConsumer();
+		val monitorObject = kafkaMonitor.getGlobalEventNLPResult();
+		String monitorTopicName = monitorObject.getTopic();
+		logger.info("监控主题为：{}", monitorTopicName);
+		String monitorGroupName = monitorObject.getGroup();
+		Integer consumeThresholdTime = monitorObject.getConsumeThresholdTime();
+		Integer produceThresholdTime = monitorObject.getProduceThresholdTime();
+		TopicMonitor(kafkaConsumer,monitorTopicName,monitorGroupName,consumeThresholdTime,produceThresholdTime);
+	}
 
 	public void TopicMonitor(org.apache.kafka.clients.consumer.KafkaConsumer kafkaConsumer,
 			String monitorTopicName,String monitorGroupName,Integer consumeThresholdTime,Integer produceThresholdTime) throws IOException {
@@ -60,7 +60,7 @@ public class SchedulerProd {
 		long consumeBeforeOffset = consumer.getOffsetByTimestamp(DateUtils.getBeforeByMinuteTime(-consumeThresholdTime));
 		long produceBeforeOffset = consumer.getOffsetByTimestamp(DateUtils.getBeforeByMinuteTime(-produceThresholdTime));
 		// 一小时前的lag
-//		long lag = consumer.getKafkaConsumerLag();
+		long lag = consumer.getKafkaConsumerLag();
 
 		// 默认正常
 		String info = String.format("kafka topic %s 监控：%s 数据正常, 当前offset：%d, %d分钟前offset：%d, %d分钟前offset：%d",
@@ -76,13 +76,13 @@ public class SchedulerProd {
 
 		// 异常情况则覆写info
 		if(consumeBeforeOffset == -1){  // 没有半小时前的消息
-//			if(lag != 0){ // 没有半小时前的消息，且队列内有积压的lag未消费则报警NLP服务异常
-//				errorInfo = String.format("kafka topic %s 监控：%s consumer异常,持续%d分钟未消费数据",
-//						monitorTopicName,
-//						DateUtils.getBeforeByHourTime(0
-//						),consumeThresholdTime);
-//				normal = false;
-//			}
+			if(lag != 0){ // 没有半小时前的消息，且队列内有积压的lag未消费则报警NLP服务异常
+				errorInfo = String.format("kafka topic %s 监控：%s consumer异常,持续%d分钟未消费数据",
+						monitorTopicName,
+						DateUtils.getBeforeByHourTime(0
+						),consumeThresholdTime);
+				normal = false;
+			}
 			// 如果没有两小时前的消息，直接报警数据推送异常
 			if(produceBeforeOffset == -1){
 				errorInfo  = String.format("kafka topic %s 监控：%s producer异常,持续%d分钟没有推送新消息",
