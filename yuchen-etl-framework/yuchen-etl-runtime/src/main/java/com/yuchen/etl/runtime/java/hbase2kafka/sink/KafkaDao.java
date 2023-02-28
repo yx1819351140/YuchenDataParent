d5e8cea354199cc087a1691da98bc69e81235bcc
qipeng Hu <qipenghu@126.com>
2023-02-28 09:54:38 +0800

    [feat][yuchen-runtime]:Hbase2kafka基础需求功能03

--- yuchen-etl-framework/yuchen-etl-runtime/src/main/java/com/yuchen/etl/runtime/java/hbase2kafka/sink/KafkaDao.java
+++ yuchen-etl-framework/yuchen-etl-runtime/src/main/java/com/yuchen/etl/runtime/java/hbase2kafka/sink/KafkaDao.java
@@ -2,11 +2,11 @@ package com.yuchen.etl.runtime.java.hbase2kafka.sink;
 
 /**
  * Copyright (C), 2023-02-20
- * FileName: KafkaDAO
+ * FileName: KafkaDao
  * Author:   HQP
  * Date:     2023/2/20 19:37
  * Description: 按照一定规则生产或者消费kafka的数据
  */
-public class KafkaDAO {
+public class KafkaDao {
 
 }
--- yuchen-etl-framework/yuchen-etl-runtime/src/main/java/com/yuchen/etl/runtime/java/hbase2kafka/sink/KafkaThreadProducer.java
+++ yuchen-etl-framework/yuchen-etl-runtime/src/main/java/com/yuchen/etl/runtime/java/hbase2kafka/sink/KafkaThreadProducer.java
@@ -2,15 +2,13 @@ package com.yuchen.etl.runtime.java.hbase2kafka.sink;
 
 
 import com.alibaba.fastjson.JSONObject;
-import org.apache.kafka.clients.producer.Callback;
-import org.apache.kafka.clients.producer.Producer;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.clients.producer.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Date;
-import java.util.Properties;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -18,54 +16,56 @@ import java.util.concurrent.Executors;
 public class KafkaThreadProducer extends  Thread
 {
    private  static  Logger logger=LoggerFactory.getLogger(KafkaThreadProducer.class);
-   //创建一个最大的线程数
-   private  final  static  int max_sise=10;
 
-   /*连接Kafka*/
-   public Properties configure()
-   {
-      Properties properties=new Properties();
-      //指定kafka的集群地址
-      properties.put("bootstrap.servers","localhost:9092");
-      //设置应答机制
-      properties.put("acks","1");
-      //批量提交大小
-      properties.put("batch.size",16384);
-      //延时提交
-      properties.put("linger.ms",1);
-      //缓充大小
-      properties.put("buffer.memory",33554432);
-      //序列化主键
-      properties.put("key.serializer","org.apache.kafka.common.serialization.StringSerializer");
-      //序列化值
-      properties.put("value.serializer","org.apache.kafka.common.serialization.StringSerializer");
-      return properties;
+   public int getMaxSize() {
+      return maxSize;
+   }
+
+   public void setMaxSize(int maxSize) {
+      this.maxSize = maxSize;
+   }
+
+   //创建一个最大的线程数
+   private  int maxSize;
+
+   private  String topic;
+
+   private List<JSONObject> jsonResults; // send messages to Kafka
+
+   private final org.apache.kafka.clients.producer.KafkaProducer<String, String> kafkaProducer;
+
+   public String getTopic() {
+      return topic;
+   }
+
+   public void setTopic(String topic) {
+      this.topic = topic;
+   }
+
+   public KafkaThreadProducer(String configPath, int maxSize) throws IOException, URISyntaxException {
+      // 根据配置文件路径创建hbase相关的配置
+      this.kafkaProducer = new KafkaHelper(configPath).createKafkaProducer();
+      this.maxSize = maxSize;
    }
 
    public  void run()
    {
-      Producer<String, String> objProducer=new org.apache.kafka.clients.producer.KafkaProducer<String, String>(this.configure());
+      Producer<String, String> objProducer=this.kafkaProducer;
+
       //模拟发送批量的数据
-      for(int i=0;i<10000;i++)
-      {
-         JSONObject jsonObject=new JSONObject();
-         jsonObject.put("id",i);
-         jsonObject.put("username","无涯");
-         jsonObject.put("city","西安");
-         jsonObject.put("age",18);
-         jsonObject.put("date",new Date().toString());
+      for(JSONObject newsJSONObject : jsonResults){
          //异步发送，调用回调函数,给主题login写入数据
-         objProducer.send(new ProducerRecord<String, String>("login", jsonObject.toJSONString()), new Callback() {
+         objProducer.send(new ProducerRecord<String, String>(topic, newsJSONObject.toJSONString()), new Callback() {
             @Override
             public void onCompletion(RecordMetadata recordMetadata, Exception e)
             {
                if(e!=null)
                {
-                  logger.error("send error:"+e.getMessage());
+                  logger.error("发送错误，信息具体为:"+e.getMessage());
                }
                else
                {
-                  logger.info("writing kafka success:"+recordMetadata.offset());
+                  logger.info("写入的数据为:"+recordMetadata.offset());
                }
             }
          });
@@ -75,19 +75,33 @@ public class KafkaThreadProducer extends  Thread
          Thread.sleep(3000);
       }catch(Exception e){
          e.printStackTrace();
+      }finally {
+         //关闭生产者的对象
+         objProducer.close();
       }
-      //关闭生产者的对象
-      objProducer.close();
    }
 
-   public static void main(String[] args)
-   {
-      ExecutorService executorService = Executors.newFixedThreadPool(max_sise);
+   public List<JSONObject> getJsonResults() {
+      return jsonResults;
+   }
+
+   public void setJsonResults(List<JSONObject> jsonResults) {
+      this.jsonResults = jsonResults;
+   }
+
+   public static void main(String[] args) throws IOException, URISyntaxException {
+      String configPath = args[0];
+
+      JSONObject jsonObject = new JSONObject();
+      jsonObject.put("context","11111111");
+      jsonObject.put("title","11111111");
+
+      KafkaThreadProducer kafkaThreadProducer = new KafkaThreadProducer(configPath,1);
+
+      ExecutorService executorService= Executors.newFixedThreadPool(kafkaThreadProducer.getMaxSize());
       //提交任务批量执行
-      executorService.submit(new KafkaThreadProducer());
+      executorService.submit(new KafkaThreadProducer(args[0],1));
       //关闭线程池
       executorService.shutdown();
-
    }
-
 }
